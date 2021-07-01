33525958c89f2f9905fcec280a2e1658f3fee8be
FixPlus <bushev.da@phystech.edu>
2021-07-01 12:44:02 +0300

    Initial commit

---
+++ include/library.h
@@ -0,0 +1,6 @@
+#ifndef VULGINE_LIBRARY_H
+#define VULGINE_LIBRARY_H
+
+void hello();
+
+#endif //VULGINE_LIBRARY_H
---
+++ main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Бушев Дмитрий on 01.07.2021.
+//
+
+#include "include/library.h"
+
+
+
+int main(int argc, char** argv){
+    hello();
+    return 0;
+}
